a0386a7c916e9358670ed1165cd5cfed8af2629f
emrecoskun096 <emre123096@gmail.com>
2023-05-27 18:39:07 +0300

    24/05

--- src/test/java/get_requests/Get05.java
+++ src/test/java/get_requests/Get05.java
@@ -1,8 +1,8 @@
 package get_requests;
 
+
 import base_urls.HerOkuAppBaseUrl;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,24 +11,23 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertTrue;
 
 public class Get05 extends HerOkuAppBaseUrl {
-       /*
+      /*
         Given
             https://restful-booker.herokuapp.com/booking
         When
             User sends a GET request to the URL
         Then
             Status code is 200
-      And
-         Among the data there should be someone whose firstname is "Sally" and last name is "Brown"
-            (Data içerisinde firstname değeri "Sally", lastname değeri "Brown" olan biri olmalı)
+	  	And
+	  		Among the data there should be someone whose firstname is "John" and last name is "Smith"
+            (Data içerisinde firstname değeri "John", lastname değeri "Smith" olan biri olmalı)
      */
 
     @Test
     public void get05() {
         //Set the url
-        //String url = "https://restful-booker.herokuapp.com/booking?firstname=Sally&lastname=Brown";
         spec.pathParam("first", "booking")
-                .queryParams("firstname", "Sally", "lastname", "Brown");
+                .queryParams("firstname", "John", "lastname", "Smith");
 
         //Set the expected data
 
@@ -36,13 +35,11 @@ public class Get05 extends HerOkuAppBaseUrl {
         Response response = given(spec).get("{first}");
         response.prettyPrint();
 
-
         //Do assertion
         response.then().statusCode(200).body("bookingid", hasSize(greaterThan(0)));
 
         //yada
         assertTrue(response.asString().contains("bookingid"));
-
 
     }
 
